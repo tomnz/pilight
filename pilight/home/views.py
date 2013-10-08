c8bedc69d216282297175a80918281503a7e3464
Tom Mitchell <tom@tom.net.nz>
2013-10-07 20:56:53 -0700

    Initial support for running a Celery task Added Bootstrap

--- pilight/home/views.py
+++ pilight/home/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from tasks import run_lights
+from celery import current_app
+
+
+def index(request):
+    return render_to_response('home/index.html')
+
+
+def start(request):
+    run_lights.apply_async(task_id='run_lights')
+    return HttpResponse()
+
+
+def stop(request):
+    current_app.control.revoke('run_lights')
+    return HttpResponse()
--- pilight/pilight/urls.py
+++ pilight/pilight/urls.py
@@ -4,14 +4,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pilight.views.home', name='home'),
-    # url(r'^pilight/', include('pilight.foo.urls')),
+# Home
+urlpatterns = patterns(
+    'home.views',
+    url(r'^/?$', 'index', name='index'),
+    url(r'^start/?$', 'start', name='start'),
+    url(r'^stop/?$', 'stop', name='stop'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+# Admin
+urlpatterns += patterns(
+    '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
