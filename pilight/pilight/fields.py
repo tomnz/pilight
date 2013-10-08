2b694abbd92fee109c02012848a4f96341941e70
Tom Mitchell <tom@tom.net.nz>
2013-10-07 19:50:54 -0700

    Add color picker widget/field

---
+++ pilight/pilight/fields.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django import forms
+from django.utils.text import capfirst
+
+
+from pilight.widgets import ColorPickerWidget
+
+class ColorField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 7
+        super(ColorField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        kwargs['widget'] = ColorPickerWidget
+        return super(ColorField, self).formfield(**kwargs)
