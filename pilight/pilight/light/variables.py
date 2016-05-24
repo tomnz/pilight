777e3a7f45b91b72c7832933b2de59eb04bb8b62
Tom <tom@tom.net.nz>
2016-05-24 10:18:50 -0400

    Improve bass detection

--- pilight/pilight/light/variables.py
+++ pilight/pilight/light/variables.py
@@ -23,9 +23,15 @@ RATE = 32000
 MAX_y = 2.0 ** (pyaudio.get_sample_size(FORMAT) * 8 - 1)
 AUDIO_SECS = 0.25
 FFT_N = 20
+
+# Frequencies for 1024 sample size @ 32000 Hz
 FFT_WEIGHTS = {
-    0: 3.0,
-    1: 1.0,
+    0: 0.2,  # 0
+    1: 0.5,  # 31.25
+    2: 0.5,  # 62.5
+    3: 0.5,  # 93.75
+    4: 0.5,  # 125
+    5: 0.2   # 156.25
 }
 PRIOR_WEIGHT = 0.7
 
@@ -47,16 +53,16 @@ class AudioVariable(Variable):
         self.val = 0.0
 
     def update(self, time):
+        chunk = None
         while self.stream.get_read_available() >= CHUNK:
             data = self.stream.read(CHUNK, exception_on_overflow=False)
-            self.frames = np.concatenate((self.frames, np.array(struct.unpack('%dh' % CHUNK, data)) / MAX_y))
+            chunk = np.array(struct.unpack('%dh' % CHUNK, data)) / MAX_y
 
-        self.frames = self.frames[int(-CHUNK):]
-        if len(self.frames) < FFT_N:
+        if not chunk:
             self.val = 0.0
             return
 
-        fft_weights = np.fft.rfft(self.frames, n=FFT_N).real
+        fft_weights = np.fft.rfft(chunk).real
 
         val = 0.0
         for idx, weight in FFT_WEIGHTS.iteritems():
