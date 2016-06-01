a84e3baf29bd86f28294fcb3052e51889ced28ed
Tom <tom@tom.net.nz>
2016-06-01 01:28:39 +0000

    Tweaks

--- pilight/pilight/light/variables.py
+++ pilight/pilight/light/variables.py
@@ -21,19 +21,9 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 MAX_y = 2.0 ** (pyaudio.get_sample_size(FORMAT) * 8 - 1)
-AUDIO_SECS = 0.25
-FFT_N = 20
-
-# Frequencies for 1024 sample size @ 32000 Hz
-FFT_WEIGHTS = {
-    0: 0.2,  # 0
-    1: 0.5,  # 31.25
-    2: 0.5,  # 62.5
-    3: 0.5,  # 93.75
-    4: 0.5,  # 125
-    5: 0.2   # 156.25
-}
-PRIOR_WEIGHT = 0.7
+AUDIO_SECS = 0.05
+AUDIO_SAMPLES = int(RATE * AUDIO_SECS)
+PRIOR_WEIGHT = 0.2
 
 class AudioVariable(Variable):
 
@@ -51,29 +41,44 @@ class AudioVariable(Variable):
 
         self.frames = np.array([0])
         self.val = 0.0
+        self.fft_weights = {}
+        self.build_weights()
+
+    def build_weights(self):
+        self.total_ffts = 0
+        for freq in np.fft.rfftfreq(AUDIO_SAMPLES, d=1.0 / RATE):
+            if freq < 90:
+                self.fft_weights[self.total_ffts] = 1.0
+                self.total_ffts += 1
+            else:
+                print self.fft_weights
+                break
 
     def update(self, time):
-        chunk = None
         while self.stream.get_read_available() >= CHUNK:
             data = self.stream.read(CHUNK, exception_on_overflow=False)
-            chunk = np.array(struct.unpack('%dh' % CHUNK, data)) / MAX_y
+            self.frames = np.concatenate((self.frames, np.array(struct.unpack('%dh' % CHUNK, data)) / MAX_y))
 
-        if chunk is None or not chunk.any():
+        if len(self.frames) < AUDIO_SAMPLES:
             self.val = 0.0
             return
 
-        fft_weights = np.fft.rfft(chunk).real
+        self.frames = self.frames[-AUDIO_SAMPLES:]
 
-        val = 0.0
-        for idx, weight in FFT_WEIGHTS.iteritems():
-            val += abs(fft_weights[idx]) * weight
+        fft_weights = np.fft.rfft(self.frames).real
 
-        self.val = self.val * PRIOR_WEIGHT + val * (1 - PRIOR_WEIGHT)
+        new_val = 0.0
+        for idx, weight in self.fft_weights.iteritems():
+            new_val += abs(fft_weights[idx]) * weight
+
+        self.val = self.val * PRIOR_WEIGHT + (new_val / self.total_ffts) * (1 - PRIOR_WEIGHT)
 
     def get_value(self):
-        return max(0.0, min(1.0, self.val - 0.5))
+        val = max(0.0, min(1.0, self.val - 0.5))
+        return val
 
     def close(self):
         self.stream.stop_stream()
         self.stream.close()
         self.pyaudio.terminate()
+
