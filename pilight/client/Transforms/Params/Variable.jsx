d283464d0e96da7247dff138fc0748314a33e273
Tom <tom@tom.net.nz>
2017-04-01 18:21:03 -0400

    Allow specifying modifiers for each variable param (#24)

--- pilight/client/Transforms/Params/Variable.jsx
+++ pilight/client/Transforms/Params/Variable.jsx
@@ -1,12 +1,31 @@
 import React, {PropTypes} from 'react';
 import {FormControl} from 'react-bootstrap';
 
+import {Float} from './Float';
+
 import css from './common.scss';
 
 
 class Variable extends React.Component {
-    onChange = (event) => {
-        this.props.onChange(event.target.value);
+    onVariableChange = (event) => {
+        this.props.onChange({
+            ...this.props.variable,
+            variable: event.target.value,
+        });
+    };
+
+    onMultiplyChange = (value) => {
+        this.props.onChange({
+            ...this.props.variable,
+            multiply: value,
+        });
+    };
+
+    onAddChange = (value) => {
+        this.props.onChange({
+            ...this.props.variable,
+            add: value,
+        });
     };
 
     render() {
@@ -17,24 +36,40 @@ class Variable extends React.Component {
                 </option>
             );
         });
-
+        console.log(this.props.variable);
         return (
-            <FormControl
-                className={css.textbox}
-                componentClass="select"
-                onChange={this.onChange}
-                value={this.props.value}
-            >
-                <option value="">None</option>
-                {variableOptions}
-            </FormControl>
+            <div>
+                <FormControl
+                    className={css.textbox}
+                    componentClass="select"
+                    onChange={this.onVariableChange}
+                    value={this.props.variable.variable}
+                >
+                    <option value="">None</option>
+                    {variableOptions}
+                </FormControl>
+                <Float
+                    onChange={this.onMultiplyChange}
+                    origValue={1}
+                    value={this.props.variable.multiply}
+                />
+                <Float
+                    onChange={this.onAddChange}
+                    origValue={0}
+                    value={this.props.variable.add}
+                />
+            </div>
         );
     }
 }
 
 Variable.propTypes = {
     onChange: PropTypes.func.isRequired,
-    value: PropTypes.string.isRequired,
+    variable: PropTypes.shape({
+        variable: PropTypes.string.isRequired,
+        multiply: PropTypes.any,
+        add: PropTypes.any,
+    }).isRequired,
     variables: PropTypes.arrayOf(
         PropTypes.shape({
             variable: PropTypes.string.isRequired,
